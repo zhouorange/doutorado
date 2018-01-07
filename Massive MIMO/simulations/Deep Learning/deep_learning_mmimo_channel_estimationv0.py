3a6c4bc32fc3c9f469f4d37af50dba7a6853e594
zz4fap <zz4fap@users.noreply.github.com>
2018-01-07 12:59:05 -0200

    fisrt version

--- Massive MIMO/simulations/Deep Learning/deep_learning_mmimo_channel_estimationv0.py
+++ Massive MIMO/simulations/Deep Learning/deep_learning_mmimo_channel_estimationv0.py
@@ -5,29 +5,39 @@ from keras.optimizers import SGD
 
 # Generate dummy data
 import numpy as np
-x_train = np.random.random((1000, 20))
-y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
-x_test = np.random.random((100, 20))
-y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+
+# Specify the number of receiving antennas at Base Stattion (BS)
+M = 100
+# Specify the number of cells.
+L = 7;
+# Specify the number of users within each one of the cells.
+K = 10;
+# Specify pilot-sequence length.
+N = K
+
+x_train = np.random.random((1000, M*N*2))
+y_train = np.random.random((1000, M*K*2))
+x_test = np.random.random((100, M*N*2))
+y_test = np.random.random((100, M*K*2))
 
 model = Sequential()
-# Dense(64) is a fully-connected layer with 64 hidden units.
+# Dense(M*N*2*4) is a fully-connected layer with M*N*2*4 hidden units.
 # in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
-model.add(Dense(64, activation='relu', input_dim=20))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
+# here, M*N*2-dimensional vectors.
+model.add(Dense(M*N*2*4, activation='tanh', input_dim=M*N*2))
+model.add(Dense(M*N*2*2, activation='tanh'))
+model.add(Dense(M*K*2, activation='linear'))
 
+# Set optimizer parameters.
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          epochs=20,
-          batch_size=128)
+# Compile model.
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train model.
+model.fit(x_train, y_train, epochs=20, batch_size=128)
+
+# Test model.
 score = model.evaluate(x_test, y_test, batch_size=128)
 
 print('Test loss:', score[0])
